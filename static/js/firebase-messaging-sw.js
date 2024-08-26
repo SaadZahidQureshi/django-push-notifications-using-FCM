2affd81cf869d0f7006f87606623d1edd444bf66
SaadZahidQureshi <saadzahid133204@gmail.com>
2024-08-26 18:08:27 +0500

    default settings.

--- static/js/firebase-messaging-sw.js
+++ static/js/firebase-messaging-sw.js
@@ -19,8 +19,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 
 // Initialize Firebase in the service worker
 firebase.initializeApp({
-  // fcm configs
- 
+  apiKey: "AIzaSyBv51-4hNiLPbReyBWeR3ncI7o98zZiIoY",
+  authDomain: "underoneroof-a0b22.firebaseapp.com",
+  projectId: "underoneroof-a0b22",
+  storageBucket: "underoneroof-a0b22.appspot.com",
+  messagingSenderId: "180332848840",
+  appId: "1:180332848840:web:8a0214c135f87560cc27fd",
+  measurementId: "G-RNP5F9N3NQ"
 });
 
 // Retrieve an instance of Firebase Messaging
